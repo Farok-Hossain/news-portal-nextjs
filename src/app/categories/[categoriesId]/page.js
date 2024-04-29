7b5398244d488037868e499bbf76b0f55e4df69f
Farok Hossain <farokafs7@gmail.com>
2024-04-29 12:08:09 +0600

    updated

--- src/app/categories/[categoriesId]/page.js
+++ src/app/categories/[categoriesId]/page.js
@@ -1,7 +1,7 @@
-const DynamicNewsPage = () => {
+const DynamicNewsPage = ({ params, searchParams }) => {
   return (
     <div>
-      <h1>Dynamic news page</h1>
+      <h1>Dynamic news page: {searchParams.category}</h1>
     </div>
   );
 };
